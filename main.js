ba8727b7fbf2877bf0f58c4f07920db5755af4a5
B4is <103561612+B4is@users.noreply.github.com>
2022-05-16 10:54:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,23 @@
-
 const popcat = document.querySelector("#popcat");
 const btn = document.querySelector("#btn");
-
-
 const openMouthImg = "./images/open.png";
 const closeMouthImg = "./images/close.png";
-
-
 const openMouthSound = new Audio("./sound/sound-open.mp3");
 const closeMouthSound = new Audio("./sound/sound-close.mp3");
-
-
 btn.addEventListener("mousedown", openMouth);
 btn.addEventListener("mouseup", closeMouth);
-
-
 btn.addEventListener("touchstart", function(e) {
     e.preventDefault();
     openMouth();
 })
-
 btn.addEventListener("touchend", function(e) {
     e.preventDefault();
     closeMouth();
 })
-
-
 function openMouth() {
     popcat.src = openMouthImg;
     openMouthSound.play();
 }
-
 function closeMouth() {
     popcat.src = closeMouthImg;
     closeMouthSound.play();
